78a50aa000f868476119da8481ef7dccb073daff
PYN <yxp7531540@gmail.com>
2019-05-14 21:30:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+    document.addEventListener("keydown", function(e){
+        if (e.key === "ArrowLeft") {
+            moveDodgerLeft()
+          }
+          if (e.key === "ArrowRight") {
+            moveDodgerRight()
+          }
+
+    })
+
+    function moveDodgerLeft() {
+        var leftNumbers = dodger.style.left.replace("px", "");
+        var left = parseInt(leftNumbers, 10);
+       
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+      }
+      function moveDodgerRight() {
+        var rightNumbers = dodger.style.left.replace("px", "");
+        var right = parseInt(rightNumbers, 10);
+       
+        if (right > 0) {
+          dodger.style.left = `${right + 1}px`;
+        }
+      }
